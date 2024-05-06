fccac5abefd878a5a3199a4c16e0cea875218f53
ShinsouBlu <93885352+ShinsouBlu@users.noreply.github.com>
2024-05-05 20:08:36 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Block from './component/Block';
+
+function App() {
+
+
+  return (
+    <Block />
+  );
+}
+
+export default App;
---
+++ src/component/Block.jsx
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from 'react'
+
+
+
+const Block = () => {
+  const[x,setX]=useState(window.innerWidth/2);
+  const[y,setY]=useState(window.innerHeight/2);
+  const[speed,setSpeed]=useState(3);
+  const[moving,setMoving]=useState(false);
+  const[direction,setDirection]=useState("up");
+
+  const changeLocation = () => {
+    console.log("Changing the location");
+    if(moving && !isTouchingSides()){
+      switch(direction){
+        case "right":console.log("going right");setX(x+speed);break;
+        case "left":setX(x-speed);break;
+        case "up":setY(y-speed);break;
+        case "down":setY(y+speed);break;
+        default:break;
+      }
+   }
+   if(isTouchingSides()){
+    console.log("X location:"+x+" Width:"+window.innerWidth)
+    console.log("X location:"+y+" Width:"+window.innerHeight)
+    console.log("Hit the wall");
+    setMoving(false);
+   }
+  }
+
+  const isTouchingSides = () => {
+    console.log("Comparing "+x+"for width and "+y+" for height");
+    return x >= window.innerWidth || x <= 0 || y >=window.innerHeight || y <= 0;
+  }
+
+  const updateSpeed = () => {
+    setSpeed(speed+5);
+  }
+
+  //Keyboard Listeners
+  useEffect(() => {
+    function keyDown(e){
+      //console.log("You just pressed " + e.code+ ", didn't you?");
+      switch(e.code){
+        case "ArrowRight":if(!moving){
+          setMoving(true);
+        }
+        setDirection('right');
+        break;
+        case "ArrowLeft":
+          if(!moving){
+            setMoving(true);
+          }
+          setDirection('left');
+          break;
+        case "ArrowUp":
+          if(!moving){
+            setMoving(true);
+          }
+          setDirection('up');
+          break;
+        case "ArrowDown":
+          if(!moving){
+            setMoving(true);
+          }
+          setDirection('down');
+          break;
+        default:break;
+      }
+    }
+    
+    document.addEventListener('keydown',keyDown);
+    //remove event listener to prevent event listener from being rerendered multiple times
+    return () => document.removeEventListener("keydown",keyDown)
+  });
+  //Create Interval to move dot around the screen
+  useEffect(() => {
+    const id=setInterval(changeLocation,50);
+    return () => clearInterval(id);
+  });
+  useEffect(() => {
+    const id=setInterval(updateSpeed,10000);
+    return () => clearInterval(id);
+  });
+  
+  
+  
+  return (
+    <div 
+    style={{
+      postion:'inline-flex',
+      width:'50px',
+      height:'50px',
+      backgroundColor:'red',
+      transform: `translate(${x}px,${y}px)`,
+    }}></div>
+  )
+}
+
+export default Block
