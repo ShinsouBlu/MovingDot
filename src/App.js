20bb8d8fe27d6b33474202e2f33e8ea978cc25a3
ShinsouBlu <93885352+ShinsouBlu@users.noreply.github.com>
2024-05-12 14:02:19 -0500

    Create Reset Button To Reset Position of Button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import { render } from '@testing-library/react';
 import './App.css';
 import Block from './component/Block';
+import React, { useState, useEffect } from 'react'
+
 
 function App() {
 
+  useEffect(() => {
+    function resetButtonPressed(e){
+      console.log('Reset Button Pressed');
+    }
+    document.getElementById("resetButton").addEventListener("click",resetButtonPressed);
+    return () => document.getElementById("resetButton").removeEventListener("click",resetButtonPressed);
+  })
 
   return (
+    <>
+    <button id="resetButton">Reset</button> 
     <Block />
+    </>
   );
 }
-
 export default App;
--- src/component/Block.jsx
+++ src/component/Block.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react'
 
 
 const Block = () => {
+  
   const[x,setX]=useState(window.innerWidth/2);
   const[y,setY]=useState(window.innerHeight/2);
   const[speed,setSpeed]=useState(3);
@@ -10,8 +11,8 @@ const Block = () => {
   const[direction,setDirection]=useState("up");
 
   const changeLocation = () => {
-    console.log("Changing the location");
-    if(moving && !isTouchingSides()){
+    //console.log("Changing the location");
+    if(moving){
       switch(direction){
         case "right":console.log("going right");setX(x+speed);break;
         case "left":setX(x-speed);break;
@@ -21,20 +22,28 @@ const Block = () => {
       }
    }
    if(isTouchingSides()){
-    console.log("X location:"+x+" Width:"+window.innerWidth)
-    console.log("X location:"+y+" Width:"+window.innerHeight)
-    console.log("Hit the wall");
+    //console.log("X location:"+x+" Width:"+window.innerWidth)
+    //console.log("X location:"+y+" Width:"+window.innerHeight)
+    //console.log("Hit the wall");
     setMoving(false);
    }
   }
 
   const isTouchingSides = () => {
-    console.log("Comparing "+x+"for width and "+y+" for height");
+    //console.log("Comparing "+x+"for width and "+y+" for height");
     return x >= window.innerWidth || x <= 0 || y >=window.innerHeight || y <= 0;
   }
 
   const updateSpeed = () => {
-    setSpeed(speed+5);
+    //console.log("Speed is being updated");
+    //setSpeed(speed+5);
+  }
+
+  const resetPosition = () => {
+    setX(window.innerWidth/2);
+    setY(window.innerHeight/2);
+    setSpeed(3);
+    setMoving(false);
   }
 
   //Keyboard Listeners
@@ -82,11 +91,23 @@ const Block = () => {
     const id=setInterval(updateSpeed,10000);
     return () => clearInterval(id);
   });
+
+  useEffect(() => {
+    const id=setInterval(updateSpeed,10000);
+    return () => clearInterval(id);
+  });
+  useEffect(() => {
+    document.getElementById("resetButton").addEventListener("click",resetPosition);
+    return () => document.getElementById("resetButton").removeEventListener("click",resetPosition);
+  });
+
+
   
   
   
   return (
-    <div 
+    <>
+    <div id="playerDot"
     style={{
       postion:'inline-flex',
       width:'50px',
@@ -94,6 +115,7 @@ const Block = () => {
       backgroundColor:'red',
       transform: `translate(${x}px,${y}px)`,
     }}></div>
+    </>
   )
 }
 
